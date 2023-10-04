2a48c8a0bb6bb8c724e44a465284cf0f0fc6a201
allamurodxakimov <xakimov2806@gmail.com>
2023-10-04 07:58:49 -0700

    loop 8

--- While08.py
+++ While08.py
@@ -6,4 +6,15 @@ def main(s):
     Returns:
         int: return answer
     """
-    return
+    a=0
+    c=0
+    k=""
+    while a<len(s):
+        if s[a].isdigit():
+            if int(s[a])%2==1:
+                k+=s[a]
+                c+=1
+        a+=1
+    return k,c
+print(main("56786543250"))
+print(main("123456"))
