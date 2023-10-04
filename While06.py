518d06810e45466dc433073f35b2ae82615ba96c
allamurodxakimov <xakimov2806@gmail.com>
2023-10-04 07:58:13 -0700

    loop 6

--- While06.py
+++ While06.py
@@ -7,3 +7,13 @@ def main(s):
     Returns:
         int: return answer
     """
+    a=0
+    c=0
+    k=""
+    while a<len(s):
+        if s[a].isalpha() and s[a]!='a' and s[a]!='e' and s[a]!='i' and s[a]!='o' and s[a]!='u' and s[a]!='A' and s[a]!='E' and s[a]!='I' and s[a]!='O' and s[a]!='U':
+            k+=s[a]
+            c+=1
+        a+=1
+    return k,c
+print(main("CodeschoolUz"))
